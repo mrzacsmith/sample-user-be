999b0e216ab6d07c0b75f08730d77c72ed7c70d5
Zac Smith <zrs3141592@gmail.com>
2020-09-19 16:14:36 -0600

    scaffold basic server with GET /

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const colors = require('colors')
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+require('dotenv').config()
+
+const server = express()
+
+server.use(cors())
+server.use(helmet())
+server.use(morgan('dev'))
+server.use(express.json)
+
+let currentTime = new Date().toLocaleString('en-US', {
+  timeZone: 'America/Denver',
+})
+
+// @desc:   confirm server is live
+// @router: GET /
+
+server.get('/', (req, res) => {
+  res.json({
+    status: 'Success',
+    message: 'Server is live',
+    dateTime: currentTime + ' MST',
+    author: 'Github: @mrzacsmith',
+  })
+})
+
+const PORT = process.env.PORT || 3225
+server.listen(PORT, () => {
+  console.log(`\n** Server is listening on port ${PORT} **\n`.rainbow)
+})
